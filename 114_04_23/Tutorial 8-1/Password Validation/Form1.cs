45d84fa305b37078c9b3db0e7fc6237f960809c3
cyc284 <A113222058@mail.shu.edu.tw>
2025-05-01 13:01:31 +0800

    114_05_01

---
+++ 114_04_23/Tutorial 8-1/Password Validation/Form1.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Validation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // NumberUpperCase よ猭钡﹃把计
+        // 肚ㄤい糶ダ计秖
+        private int NumberUpperCase(string str)
+        {
+            
+        }
+
+        // NumberLowerCase よ猭钡﹃把计
+        // 肚ㄤい糶ダ计秖
+        private int NumberLowerCase(string str)
+        {
+            
+        }
+
+        // NumberDigits よ猭钡﹃把计
+        // 肚ㄤい计じ计秖
+        private int NumberDigits(string str)
+        {
+            
+        }
+
+        private void checkPasswordButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 闽超虫
+            this.Close();
+        }
+    }
+}
